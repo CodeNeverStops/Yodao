3b5a7818abf881b06c5b2e688416e3e0cc1f67ba
Wayne.You <lokiwuxi@gmail.com>
2016-01-27 17:41:00 +0800

    format

--- Yodao.php
+++ Yodao.php
@@ -1,21 +1,21 @@
 <?php
 spl_autoload_register(function ($className) {
-	$fileName = '';
-	$namespace = '';
+    $fileName = '';
+    $namespace = '';
 
-	$basePath = dirname(__FILE__);
+    $basePath = dirname(__FILE__);
 
-	if (false !== ($lastNsPos = strripos($className, '\\'))) {
-		$namespace = substr($className, 0, $lastNsPos);
-		$className = substr($className, $lastNsPos + 1);
-		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-	}
-	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-	$fullFileName = $basePath . DIRECTORY_SEPARATOR . $fileName;
+    if (false !== ($lastNsPos = strripos($className, '\\'))) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fullFileName = $basePath . DIRECTORY_SEPARATOR . $fileName;
 
-	if (file_exists($fullFileName)) {
-		require $fullFileName;
-	} else {
-		echo 'Class "'.$className.'" does not exist.';
-	}
+    if (file_exists($fullFileName)) {
+        require $fullFileName;
+    } else {
+        echo 'Class "'.$className.'" does not exist.';
+    }
 }); 
